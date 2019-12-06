e8d5dfcac808715196ab0a37ab9a0152ff0b571f
Glenn Lewis <6598971+gmlewis@users.noreply.github.com>
2019-12-05 23:21:18 -0500

    Add 18-transparent-texture

---
+++ examples/18-transparent-texture/main.go
@@ -0,0 +1,83 @@
+// 18-transparent-texture is a port to Go of the Babylon example located here:
+// https://www.babylonjs-playground.com/#20OAV9#17
+// and linked from here:
+// https://doc.babylonjs.com/babylon101/materials
+package main
+
+import (
+	"math"
+	"syscall/js"
+
+	"github.com/gmlewis/go-babylonjs/babylon"
+)
+
+func main() {
+	doc := js.Global().Get("document")
+	canvas := doc.Call("getElementById", "renderCanvas") // Get the canvas element
+
+	b := babylon.New()
+
+	engine := b.NewEngine(canvas, &babylon.NewEngineOpts{Antialias: babylon.Bool(true)}) // Generate the BABYLON 3D engine
+
+	/******* Add the create scene function ******/
+	createScene := func() *babylon.Scene {
+		scene := b.NewScene(engine, nil)
+		camera := b.NewArcRotateCamera("Camera", -math.Pi/2, 3*math.Pi/8, 5, b.Vector3().Zero(), scene, nil)
+		camera.AttachControl(canvas, &babylon.ArcRotateCameraAttachControlOpts{NoPreventDefault: Bool(true)})
+
+		//Light direction is up and left
+		light := b.NewHemisphericLight("hemiLight", b.NewVector3(-1, 1, 0), scene)
+		light.SetDiffuse(b.NewColor3(1, 0, 0))
+		light.SetSpecular(b.NewColor3(0, 1, 0))
+		light.SetGroundColor(b.NewColor3(0, 1, 0))
+
+		redMat := b.NewStandardMaterial("redMat", scene)
+		redMat.SetDiffuseTexture(b.NewTexture("textures/grass.png", scene, nil).BaseTexture)
+
+		greenMat := b.NewStandardMaterial("greenMat", scene)
+		greenMat.SetAmbientTexture(b.NewTexture("textures/grass.png", scene, nil).BaseTexture)
+		greenMat.JSObject().Set("alpha", 0.5)
+
+		//Red
+		sphere1 := b.CreateSphere("sphere1", nil, scene)
+		sphere1.JSObject().Set("material", redMat.JSObject())
+		sphere1.Position().SetZ(1.5)
+
+		//Green Transparent
+		sphere2 := b.CreateSphere("sphere2", nil, scene)
+		sphere2.JSObject().Set("material", greenMat.JSObject())
+
+		return scene
+	}
+	/******* End of the create scene function ******/
+
+	scene := createScene() //Call the createScene function
+
+	// Register a render loop to repeatedly render the scene
+	engine.RunRenderLoop(func() {
+		scene.Render(nil)
+	})
+
+	// Watch for browser/canvas resize events
+	window := js.Global().Get("window")
+	cb := js.FuncOf(func(this js.Value, args []js.Value) interface{} {
+		engine.Resize()
+		return nil
+	})
+	// Note that "cb" is never released since it is needed for resizing.
+	window.Call("addEventListener", "resize", cb)
+
+	// prevent program from terminating
+	c := make(chan struct{}, 0)
+	<-c
+}
+
+// Bool returns the pointer to the provided bool.
+func Bool(v bool) *bool {
+	return &v
+}
+
+// Float64 returns the pointer to the provided float64.
+func Float64(v float64) *float64 {
+	return &v
+}
